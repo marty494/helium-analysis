c8a61a665b8aa7d48e6726d87163bb674c3199c7
marty494 <martinjohndavey@gmail.com>
2021-12-05 13:31:22 +0000

    Added a simple home and about page

--- src/server.py
+++ src/server.py
@@ -1,9 +1,15 @@
 from flask import Flask
+from flask import Flask, render_template
+
 server = Flask(__name__)
 
 @server.route('/')
-def hello():
-    return 'Hello World!'
+def home():
+    return render_template('home.html')
+
+@server.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
     server.run(host='0.0.0.0')
