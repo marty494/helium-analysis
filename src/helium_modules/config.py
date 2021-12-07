c66f494a070fabc0e15b3786276fa0dac120bc16
marty494 <martinjohndavey@gmail.com>
2021-12-07 21:56:06 +0000

    New config module

---
+++ src/helium_modules/config.py
@@ -0,0 +1,103 @@
+import os
+import json
+import helium_modules.helium_api as api
+import helium_modules.kibana as kibana
+import logging
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
+
+#
+# LOAD IN THE CONFIG FILE WHICH CONTAINS THE HOTSPOT ADDRESSES TO BE PROCESSED
+#
+def get_hotspots():
+    configPath = "/data/config.json"
+    configContents = open(configPath).read()
+    configJson = json.loads(configContents)
+
+    return configJson
+
+
+def get_hotspot_details(hotspot_address):
+    hotspot_config = kibana.get_document('helium-config', hotspot_address)
+
+    if hotspot_config['found'] == True:
+        hotspot_details = extract_details_from_config(hotspot_config)
+    else:
+        hotspot_details = create_hotspot_config(hotspot_address)
+
+    return hotspot_details
+
+
+#
+# EXTRACT HOTSPOT DETAILS FROM CONFIG JSON
+#
+def extract_details_from_config(hotspot_config):
+    config = hotspot_config['_source']
+
+    hotspot_details = {
+        'name': config['name'],
+        'born_date': config['born_date'],
+        'processed_date': config['processed_date'],
+        'activity_count': config['activity_count']
+    }
+
+    return hotspot_details
+
+#
+# INSERT NEW HOTSPOT DETAILS INTO CONFIG
+#
+def create_hotspot_config(hotspot_address):
+    hotspot_data = api.get_hotspot_data(hotspot_address)
+    
+    if hotspot_data['name'] == '':
+        raise KeyError('create_hotspot_config() error: ' + str(hotspot_data))
+
+    activity_count = api.get_hotspot_activity_count(hotspot_address)
+
+    logger.debug('Adding hotspot details to config for address: ' + hotspot_address)
+
+    hotspot_details = {
+        'name': hotspot_data['name'],
+        'born_date': hotspot_data['timestamp_added'],
+        'processed_date': hotspot_data['timestamp_added'],
+        'activity_count': activity_count
+    }
+    
+    response = kibana.write_document('helium-config', hotspot_details, hotspot_address)
+
+    logger.debug('WANKO: ' + str(response))
+
+    if response['status'] != 200:
+        raise Exception('status: ' + str(response['status']))
+
+    return hotspot_details
+
+
+#
+# USED BY THE HISTORIC ACTIVITY PROCESSING
+# WHEN ALL ACTIVITY HAS BEEN FETCHED BACK TO THE HOTSPOT BIRTH DATE
+# THEN THE HISTORIC PROCESSING WILL NO LONGER BE PERFORMED FOR THAT 
+#
+def update_hotspot_earliest_processed_date(table, address, date):
+    response = table.update_item(
+        Key={
+            'address': address
+        },
+        UpdateExpression="set earliest_processed_date=:lpd",
+        ExpressionAttributeValues={
+            ':lpd': date
+        },
+        ReturnValues="UPDATED_NEW"
+    )
+
+    if 'ResponseMetadata' in response:
+        if 'HTTPStatusCode' in response['ResponseMetadata']:
+            if response['ResponseMetadata']['HTTPStatusCode'] == 200:
+                logger.debug("SUCCESS - update_hotspot_earliest_processed_date - Updating: " + str(date))
+                return True
+
+    logger.error("FAILED - update_hotspot_earliest_processed_date - Updating: " + str(response))
+    return False
+
+
